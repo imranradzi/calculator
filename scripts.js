1d00d8aaf0e0b1e758d85b3954ed1042698a8737
Imran <imranradziii@gmail.com>
2022-06-25 16:57:06 +0100

    made it so we can perform operations on 2 numbers at a time, as well as clear button which clears the numbers entered in the calculator

--- scripts.js
+++ scripts.js
@@ -25,3 +25,43 @@ function operate(a, b, operator) {
   }
   if (operator in operatorList) return operatorList[operator];
 }
+
+let numDisplay = document.querySelector('#numberDisplay');
+let numDisplayContent = '';
+let displayValue = 0;
+let currentOperator;
+const nums = document.querySelectorAll('.row > .nums');
+const numsArray = Array.from(nums);
+const ops = document.querySelectorAll('.row > .ops');
+const opsArray = Array.from(ops);
+const eq = document.querySelector('.eq');
+const allClear = document.querySelector('.allClear');
+
+numsArray.forEach(num => {
+  num.addEventListener('click', function() {
+    numDisplayContent = numDisplayContent + num.textContent;
+    numDisplay.textContent = numDisplayContent;
+  })
+});
+
+opsArray.forEach(op => {
+  op.addEventListener('click', function() {
+    displayValue = parseInt(numDisplay.textContent);
+    numDisplay.textContent = op.textContent;
+    numDisplayContent = '';
+    currentOperator = op.textContent;
+  })
+});
+
+eq.addEventListener('click', function() {
+  result = operate(displayValue,
+              parseInt(numDisplayContent), currentOperator);
+  numDisplay.textContent = result.toString();
+  displayValue = result;
+});
+
+allClear.addEventListener('click', function() {
+  numDisplayContent = '';
+  displayValue = 0;
+  numDisplay.textContent = numDisplayContent;
+});
