795bd1ffada1e9461f0c35ed83e7d678cba5bd81
Imran <imranradziii@gmail.com>
2022-06-25 23:40:11 +0100

    modified scripts.js so that calculator can do operations with negative numbers, also changed so that trailing zeros should be removed

--- scripts.js
+++ scripts.js
@@ -38,11 +38,15 @@ secondNumDisplay.textContent = '';
 let operandArray = [];
 let operatorArray = [];
 
+let negative = false;
+
 const nums = document.querySelectorAll('.row > .nums');
 const numsArray = Array.from(nums);
 
 const ops = document.querySelectorAll('.row > .ops');
 const opsArray = Array.from(ops);
+
+
 
 const eq = document.querySelector('.eq');
 
@@ -51,45 +55,61 @@ const allClear = document.querySelector('.allClear');
 // functionality for number buttons
 numsArray.forEach(num => {
   num.addEventListener('click', function() {
-    numDisplayContent = numDisplayContent + num.textContent;
-
+    if (negative === true) {
+      numDisplayContent = `-${num.textContent}`;
+      negative = false;
+    } else {
+      numDisplayContent = numDisplayContent + num.textContent;
+    }
+    
     // appends number clicked onto the display
     numDisplay.textContent = numDisplayContent;
     secondNumDisplay.textContent += `${num.textContent}`;
   })
 });
 
+
+
 // functionality for operator buttons
 opsArray.forEach(op => {
   op.addEventListener('click', function() {
-    // add the current number/operator on display to operandArray/operatorArray
-    operandArray.push(parseFloat(numDisplay.textContent));
-    operatorArray.push(op.textContent);
-    secondNumDisplay.textContent += ` ${op.textContent} `;
+    // if we want to use negative numbers
+    if (op.textContent === '-' && operandArray[0] && operatorArray[0]) {
+        numDisplayContent = numDisplayContent + op.textContent;
+        negative = true;
+        secondNumDisplay.textContent += ` ${op.textContent} `;
+    } else {
 
-    let result;
-    // if, for example, we previously clicked 2, +, 2 then + again,
-    // we add (or subtract, multiply, etc.) the previous two numbers
-    // and current display is changed to the result
-    if (operandArray.length === 2) {
-      // checks if we are dividing by zero
-      if (operandArray[1] === 0 && operatorArray[0] === '/') {
+      // add the current number/operator on display to operandArray/operatorArray
+      operandArray.push(parseFloat(numDisplay.textContent));
+      operatorArray.push(op.textContent);
+      secondNumDisplay.textContent += ` ${op.textContent} `;
 
-        // resets calculator, displaying error message before resetting
-        errorMessage();
-      } else {
-        // by this point our operandArray contains two operands, and
-        // two operators, of which we select the first operator
-        // since that is the operator with precedence (we clicked it first)
-        result = operate(operandArray[0], operandArray[1], operatorArray[0])
-                  .toPrecision(5);
-        numDisplay.textContent = result;
+      let result;
+      // if, for example, we previously clicked 2, +, 2 then + again,
+      // we add (or subtract, multiply, etc.) the previous two numbers
+      // and current display is changed to the result
+      if (operandArray.length === 2) {
+        // checks if we are dividing by zero
+        if (operandArray[1] === 0 && operatorArray[0] === '/') {
 
-        // resets the two arrays to be used for the next calculation
-        operandArray = [parseFloat(numDisplay.textContent)];
-        operatorArray = [operatorArray[1]];
+          // resets calculator, displaying error message before resetting
+          errorMessage();
+        } else {
+          // by this point our operandArray contains two operands, and
+          // two operators, of which we select the first operator
+          // since that is the operator with precedence (we clicked it first)
+          result = operate(operandArray[0], operandArray[1], operatorArray[0])
+                    .toPrecision(5);
+          numDisplay.textContent = (result % 1) ? (result / 1) : parseInt(result);
+
+          // resets the two arrays to be used for the next calculation
+          operandArray = [parseFloat(numDisplay.textContent)];
+          operatorArray = [operatorArray[1]];
+        }
       }
     }
+    
 
     // set numDisplayContent to empty string, as when we press another number,
     // the next operand shouldn't be appended to the current operand
@@ -108,7 +128,8 @@ eq.addEventListener('click', function() {
       result = operate(operandArray[0], operandArray[1], operatorArray[0]);
       operandArray = [];
       operatorArray = [];
-      numDisplay.textContent = result.toPrecision(5);
+      result = result.toPrecision(5);
+      numDisplay.textContent = (result % 1) ? (result / 1) : parseInt(result);
     }
   }
 });
